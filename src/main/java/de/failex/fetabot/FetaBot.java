a0e9f832859bdce015987721fc57ac8f1ef4a3c3
Felix Naumann <felix@emilnaumann.de>
2017-06-21 19:08:45 +0200

    Added bot base

--- src/main/java/de/failex/fetabot/FetaBot.java
+++ src/main/java/de/failex/fetabot/FetaBot.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FetaBot {
@@ -11,6 +12,10 @@ public class FetaBot {
     static File config = new File("config.json");
     static Gson gson = new Gson();
     static ConfigManager cfg;
+
+    static final String PRGNAME = "FeTaBot";
+    static final String VERSION = "1.0-SNAPSHOT";
+    static final String AUTHOR = "failex234";
 
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -25,7 +30,7 @@ public class FetaBot {
                     }
                     in.close();
 
-                    cfg = gson.fromJson(temp.toString(), ConfigManager.class);#
+                    cfg = gson.fromJson(temp.toString(), ConfigManager.class);
 
                     log("Config loaded!");
 
@@ -45,8 +50,71 @@ public class FetaBot {
         } else {
             if (config.exists()) {
                 log("Config found but also arguments found. Overwriting config...");
+                cfg = new ConfigManager();
+                if (args.length != 4) {
+                    log("Too few arguments! Please restart with enough arguments!");
+                    System.exit(1);
+                } else {
+                    cfg.setUser(args[0]);
+                    cfg.setOauth(args[1]);
+                    cfg.setChannel(args[2]);
+                    cfg.setOwner(args[3]);
+
+                    log("Overwriting config but copying mods and commands...");
+                    try {
+                        PrintWriter writer = new PrintWriter(config);
+                        ConfigManager temp = cfg;
+
+                        Scanner in = new Scanner(new FileReader(config));
+                        StringBuilder tempsb = new StringBuilder();
+
+                        while (in.hasNext()) {
+                            tempsb.append(in.next());
+                        }
+                        in.close();
+
+                        temp = gson.fromJson(temp.toString(), ConfigManager.class);
+
+                        cfg.setMods(temp.getMods());
+                        cfg.setCommands(temp.getCommands());
+
+                        config.delete();
+
+                        writer.write(gson.toJson(cfg));
+
+                        writer.close();
+
+                    } catch (Exception e) {
+                        log("Something went wrong! Unable to continue, exiting now...");
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+
+                }
+            } else {
+                log("Config not found but found arguments. Creating config...");
+
+                cfg = new ConfigManager();
+
+                cfg.setUser(args[0]);
+                cfg.setOauth(args[1]);
+                cfg.setChannel(args[2]);
+                cfg.setOwner(args[3]);
+                try {
+                    PrintWriter writer = new PrintWriter(config);
+                    writer.write(gson.toJson(cfg));
+                    writer.close();
+                    log("Config written!");
+                } catch (Exception e) {
+                    log("Something went wrong! Unable to continue, exiting now...");
+                    e.printStackTrace();
+                    System.exit(1);
+                }
             }
         }
+
+        log (PRGNAME + " Version " + VERSION + " by " + AUTHOR + " starting up...");
+        //TODO
     }
 
     public static void log(String msg) {
